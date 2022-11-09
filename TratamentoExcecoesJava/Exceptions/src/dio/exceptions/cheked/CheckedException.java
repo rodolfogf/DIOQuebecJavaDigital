59e9278c0bc387b9acfe60b66f2029930dd59ebc
Rodolfo Faria <rodolfogf@gmail.com>
2022-11-09 00:05:48 -0300

    Tratamento de Exceções em Java - Checked

--- TratamentoExcecoesJava/Exceptions/src/dio/exceptions/cheked/CheckedException.java
+++ TratamentoExcecoesJava/Exceptions/src/dio/exceptions/cheked/CheckedException.java
@@ -1,5 +1,38 @@
 package dio.exceptions.cheked;
 
+import javax.swing.*;
+import java.io.*;
+
 public class CheckedException {
-    
+    public static void main(String[] args){
+
+        String nomeArquivo = "teste.txt";
+
+        try{
+            imprimirArquivo(nomeArquivo);
+        }catch (FileNotFoundException e){
+            JOptionPane.showMessageDialog(null, "Arquivo não encontrado");
+        }catch (IOException e){
+            JOptionPane.showMessageDialog(null, "Erro ao tentar abrir o arquivo");
+            e.printStackTrace();
+        }
+    }
+
+    public static void imprimirArquivo(String nmarquivo) throws IOException {
+
+        File arquivo = new File(nmarquivo);
+
+        BufferedReader br = new BufferedReader(new FileReader(arquivo.getName()));
+        String linha = br.readLine();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do{
+            bw.write(linha);
+            bw.newLine();
+            linha = br.readLine();
+        }while(linha != null);
+        bw.flush();
+        br.close();
+    }
 }
